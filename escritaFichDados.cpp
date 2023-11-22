5b286f02122c57278c0c2412bbdedc30d6523e63
Maria <conceicao.martinho.estudo@gmail.com>
2023-11-22 18:06:50 +0000

    Atualização

---
+++ escritaFichDados.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <clocale>
+#include <fstream>
+
+using namespace std;
+
+class livros{
+    
+};
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+
+    return ;
+}
